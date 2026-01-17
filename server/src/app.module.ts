a4bb37708d88f97b8823888ddf756ea3fdcd0f9c
Nfor Nde <xinodeprinz@gmail.com>
2026-01-17 03:53:54 +0100

    Made public folder public so as to serve images publicly

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module.js';
 import { UserModule } from './user/user.module.js';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'public'),
+    }),
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +12,7 @@ async function bootstrap() {
       stopAtFirstError: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
