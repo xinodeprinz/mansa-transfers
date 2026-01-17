e0ae2683c715c9686d30680d9a5e1e2774014394
Nfor Nde <xinodeprinz@gmail.com>
2026-01-17 02:05:57 +0100

    Created user module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module.js';
+import { UserModule } from './user/user.module.js';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule],
 })
 export class AppModule {}
